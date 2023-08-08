14df8e74b7145c11ddcdf118a3eefcd5f69e30bf
Antony <86053464+AntonyLoose@users.noreply.github.com>
2023-08-09 00:34:39 +1000

    Add app.tsx

--- demo/app/App.tsx
+++ demo/app/App.tsx
@@ -1,20 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { DrawerNavigator } from './src/navigators/DrawerNavigator';
+import { TabNavigator } from './src/navigators/TabNavigator';
+import { Tab } from './src/navigators/types';
+import { Account } from './src/screens/Account';
+import { Home } from './src/screens/home/Home';
+import { HomeSidebar } from './src/screens/home/HomeSidebar';
+import { Settings } from './src/screens/Settings';
+import { colours } from './src/styles';
 
 export default function App() {
+
+  const tabs: Tab[] = [
+    {
+      label: "Home",
+      screen: {
+        component: Home,
+        title: "Home"
+      },
+      icon: {
+        focused: "home",
+        unfocused: "home-outline",
+      },
+      sidebar: {
+        component: HomeSidebar,
+        title: "Homes"
+      },
+    },
+    {
+      label: "Account",
+      screen: {
+        component: Account,
+        title: "Account"
+      },
+      icon: {
+        focused: "account",
+        unfocused: "account-outline",
+      }
+    },
+    {
+      label: "Settings",
+      screen: {
+        component: Settings,
+        title: "Settings"
+      },
+      icon: {
+        focused: "cog",
+        unfocused: "cog-outline",
+      }
+    }
+  ]
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* <TabNavigator 
+        tabs={tabs}
+        tabbarStyle={{
+          backgroundColor: colours.background
+        }}
+        headerStyle={{
+          backgroundColor: colours.background
+        }}
+        titleStyle={{
+          color: colours.text
+        }}
+        iconColor={colours.text}
+        labelStyle={{
+          color: colours.text
+        }}
+        backIcon={{
+          icon: "chevron-left",
+          color: colours.text
+        }}
+      /> */}
+      <DrawerNavigator 
+        tabs={tabs} 
+        drawerTitle={"Navigator"}
+        drawerStyle={{
+          backgroundColor: colours.background
+        }}  
+        drawerTabFocusedStyle={{
+          style: {
+            backgroundColor: colours.card
+          }
+        }}
+        drawerTitleStyle={{
+          color: colours.text
+        }}
+        toggleDrawerIcon={{
+          color: colours.text
+        }}
+        labelStyle={{
+          color: colours.text
+        }} 
+        tabIconStyle={{
+          focusedColor: colours.text,
+          unFocusedColor: colours.text
+        }}
+        sidebarStyle={{
+          style: {
+            backgroundColor: colours.background,
+            borderColor: colours.card
+          },
+          titleStyle: {
+            color: colours.text
+          }
+        }}
+        screenHeaderStyle={{
+          backgroundColor: colours.background
+        }}
+        screenTitleStyle={{
+          color: colours.text
+        }}
+        borderColor={colours.card}
+      />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
